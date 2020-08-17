22e9ada18757b93cb34dd56e1a72f25832ef2908
ViktoriiaLebedeva <viktoriia_lebedeva@epam.com>
2020-08-17 13:55:46 +0300

    EPMDJ-3095 Added support for latest version of Spock(2.0-M3-groovy-3.0)

--- src/runtimeMain/kotlin/com/epam/drill/test/agent/penetration/testing/spock/Spock.kt
+++ src/runtimeMain/kotlin/com/epam/drill/test/agent/penetration/testing/spock/Spock.kt
@@ -1,22 +1,36 @@
 package com.epam.drill.test.agent.penetration.testing.spock
 
-import com.epam.drill.test.agent.ThreadStorage
-import com.epam.drill.test.agent.penetration.AbstractTestStrategy
-import javassist.CtClass
+import com.epam.drill.test.agent.*
+import com.epam.drill.test.agent.penetration.*
+import javassist.*
 
 class Spock : AbstractTestStrategy() {
     override val id: String
         get() = "spock"
 
+    private val baseRunner = "org.spockframework.runtime.BaseSpecRunner"
+    private val platformRunner = "org.spockframework.runtime.PlatformSpecRunner"
+
     override fun permit(ctClass: CtClass): Boolean {
-       return ctClass.name == "org.spockframework.runtime.BaseSpecRunner"
+        return ctClass.name == baseRunner || ctClass.name == platformRunner
     }
 
     override fun instrument(ctClass: CtClass): ByteArray? {
-        ctClass.getDeclaredMethod("createMethodInfoForDoRunFeature")
-            .insertBefore("""
-                 ${ThreadStorage::class.java.name}.INSTANCE.${ThreadStorage::memorizeTestName.name}(currentFeature.getDescription().toString());
-            """.trimIndent())
+        val createMethodInfo = ctClass.getDeclaredMethod("createMethodInfoForDoRunFeature")
+        when (ctClass.name) {
+            baseRunner -> createMethodInfo.insertBefore(
+                """
+                    String testName = currentFeature.getDescription().toString();
+                 ${ThreadStorage::class.java.name}.INSTANCE.${ThreadStorage::memorizeTestName.name}(testName);
+            """.trimIndent()
+            )
+            platformRunner -> createMethodInfo.insertBefore(
+                """
+                    String testName = $1.getCurrentFeature().getName() + "("+ $1.getCurrentFeature().getParent().getName()+")";
+                 ${ThreadStorage::class.java.name}.INSTANCE.${ThreadStorage::memorizeTestName.name}(testName);
+            """.trimIndent()
+            )
+        }
         return ctClass.toBytecode()
     }
 
