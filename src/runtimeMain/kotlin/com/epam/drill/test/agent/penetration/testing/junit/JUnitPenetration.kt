d2a7b012ac1546048e0b707b4a7f592f13856f45
IgorKey <Igor_Kuzminykh@epam.com>
2020-09-07 11:00:58 +0300

    EPMDJ-3155 Supported cucumber (bugfix with junit4runner)

--- src/runtimeMain/kotlin/com/epam/drill/test/agent/penetration/testing/junit/JUnitPenetration.kt
+++ src/runtimeMain/kotlin/com/epam/drill/test/agent/penetration/testing/junit/JUnitPenetration.kt
@@ -1,11 +1,24 @@
 package com.epam.drill.test.agent.penetration.testing.junit
 
-import com.epam.drill.test.agent.penetration.AnnotationStrategy
+import com.epam.drill.test.agent.ThreadStorage
+import com.epam.drill.test.agent.penetration.AbstractTestStrategy
+import javassist.CtClass
 
-class JUnitPenetration : AnnotationStrategy() {
+class JUnitPenetration : AbstractTestStrategy() {
     override val id: String = "junit"
+    override fun permit(ctClass: CtClass): Boolean {
+        return ctClass.name == "org.junit.runners.ParentRunner"
+    }
 
-    init {
-        supportedAnnotations.add("@org.junit.Test")
+    override fun instrument(ctClass: CtClass): ByteArray? {
+        val runChild = ctClass.getDeclaredMethod("runLeaf")
+        runChild.insertBefore(
+            """
+                String drillTestName = $2.getDisplayName();
+                ${ThreadStorage::class.java.name}.INSTANCE.${ThreadStorage::memorizeTestName.name}(drillTestName);
+                
+                """.trimIndent()
+        )
+        return ctClass.toBytecode()
     }
 }
