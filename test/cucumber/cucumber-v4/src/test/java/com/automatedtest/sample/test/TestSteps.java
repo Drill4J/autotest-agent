c1ceae80863cc8244b6b110318b34fb92caa9c89
Roma <roma.ivanov1999@mail.ru>
2020-12-30 17:51:00 +0300

    EPMDJ-5770 Fixed cucumber and spock tests

--- test/cucumber/cucumber-v4/src/test/java/com/automatedtest/sample/test/TestSteps.java
+++ test/cucumber/cucumber-v4/src/test/java/com/automatedtest/sample/test/TestSteps.java
@@ -5,7 +5,6 @@ import io.cucumber.java.*;
 import io.cucumber.java.en.*;
 import com.epam.drill.test.common.*;
 
-import java.io.UnsupportedEncodingException;
 
 
 public class TestSteps {
@@ -28,7 +27,7 @@ public class TestSteps {
     }
 
     @Before
-    public void saveScenarioName(Scenario scenario) throws UnsupportedEncodingException {
+    public void saveScenarioName(Scenario scenario) {
         actualTestName = UtilKt.urlEncode(scenario.getName());
     }
 }
--- test/spock/build.gradle.kts
+++ test/spock/build.gradle.kts
@@ -1,9 +1,9 @@
 apply(plugin = "groovy")
 
 dependencies {
+    implementation(project(":test:commonTest"))
     testImplementation("org.codehaus.groovy:groovy-all:2.4.11")
     testImplementation("org.spockframework:spock-core:1.1-groovy-2.4")
-    implementation(project(":test:commonTest"))
 }
 
 tasks.withType<Test> {
