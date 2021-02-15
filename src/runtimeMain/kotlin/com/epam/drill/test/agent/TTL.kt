75cf4d7b4f9816fd5ab0621b0ff4f722b8239806
Roma <roma.ivanov1999@mail.ru>
2021-02-15 15:34:36 +0300

    EPMDJ-6255 Selenium tests must be thread safe

--- src/runtimeMain/kotlin/com/epam/drill/test/agent/TTL.kt
+++ src/runtimeMain/kotlin/com/epam/drill/test/agent/TTL.kt
@@ -1,5 +1,7 @@
 package com.epam.drill.test.agent
 
+import com.epam.drill.test.agent.instrumentation.http.selenium.*
+
 const val TEST_NAME_HEADER = "drill-test-name"
 const val SESSION_ID_HEADER = "drill-session-id"
 
@@ -8,6 +10,7 @@ val TEST_NAME_CALC_LINE = "\"$TEST_NAME_HEADER\", $TEST_NAME_VALUE_CALC_LINE"
 val SESSION_ID_VALUE_CALC_LINE = "${ThreadStorage::class.qualifiedName}.INSTANCE.${ThreadStorage::sessionId.name}()"
 val SESSION_ID_CALC_LINE = "\"$SESSION_ID_HEADER\", $SESSION_ID_VALUE_CALC_LINE"
 val IF_CONDITION = "$TEST_NAME_VALUE_CALC_LINE != null && $SESSION_ID_VALUE_CALC_LINE != null"
+val IS_HEADER_ADDED = "${DevToolsClientThreadStorage::class.java.name}.INSTANCE.${DevToolsClientThreadStorage::isHeadersAdded.name}()"
 
 @Suppress("RedundantOverride")
 class TTL : InheritableThreadLocal<String>() {
