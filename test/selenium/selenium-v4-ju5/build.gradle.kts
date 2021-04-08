cd887c5b33253a68eacbff1d45ac26d63eb09d1d
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-04-09 01:32:24 +0300

    upgrade to selenide:5.20.1 etc.

--- test/selenium/selenium-v4-ju5/build.gradle.kts
+++ test/selenium/selenium-v4-ju5/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-    testImplementation("com.codeborne:selenide:5.12.2")
-    testImplementation("com.browserup:browserup-proxy-core:2.1.1")
-    testImplementation("org.testcontainers:junit-jupiter:1.14.3")
-    testImplementation("org.testcontainers:testcontainers:1.14.3")
+    testImplementation("com.codeborne:selenide:5.20.1")
+    testImplementation("com.browserup:browserup-proxy-core:2.1.2")
+    testImplementation("org.testcontainers:junit-jupiter:1.15.2")
+    testImplementation("org.testcontainers:testcontainers:1.15.2")
 }
